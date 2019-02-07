98a9c0c4201692a21375b3346e1e0e7c8c996b16
Kevan Rahav <kevan.or@gmail.com>
2019-02-06 20:44:35 -0700

    Added paddle, block and ball to scene Added colliders to block, ball and paddle Added Bounce material to ball Added collison triger box, to detact ball going off screen Added Paddle movement (following mouse X position relative to screen

---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthUnits = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //X position relative to screen size
+        float mousePosX = Input.mousePosition.x / Screen.width * screenWidthUnits;
+        Vector2 padPosition = new Vector2(mousePosX, transform.position.y);
+        transform.position = padPosition;
+    }
+}
